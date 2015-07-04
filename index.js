5863d0b63b8d74619025ff84b95a9f66d5335a7b
Koki Takahashi <hakatasiloving@gmail.com>
2015-07-04 23:53:13 +0900

    Rebuild

--- index.js
+++ index.js
@@ -1,11 +1,139 @@
 (function() {
-  var availableParts, randint, resetParts, submit,
+  var availableParts, character, getRandomParts, randint, resetParts, submit, totalWeight, weight,
     hasProp = {}.hasOwnProperty;
 
-  availableParts = ["一", "口", "十", "木", "ノ", "丶", "丿", "艹", "シ", "大", "八", "𠆢", "イ", "月", "又", "⺘", "女", "金", "火", "宀", "ク", "亠", "糸", "儿", "虫", "山", "灬", "言", "勹", "厶", "貝", "冖", "心", "⻖", "冂", "⺖", "⺮", "土", "夂", "人", "石", "寸", "隹", "攵", "辶", "鳥", "力", "⺉", "厂", "皿", "艮", "广", "夕", "匕", "尸", "車", "耳", "刀", "几", "子", "巾", "钅", "工", "足", "疒", "止", "丁", "衤", "馬", "方", "⺨", "小", "羽", "立", "雨", "門", "⺫", "彳", "䒑", "廾", "戈", "彡", "酉", "匚", "斤", "欠", "虍", "贝", "弓", "纟", "讠", "幺", "爫", "龷", "示", "⺊", "㐅", "冫", "西", "兀", "豕", "㔾", "⺌", "衣", "ネ", "⺧", "革", "户", "丂", "巳", "臼", "比", "戊", "舟", "巛", "マ", "龰", "辛", "毛", "二"];
+  availableParts = {
+    "一": 9034,
+    "口": 7472,
+    "十": 3935,
+    "木": 2270,
+    "ノ": 1899,
+    "丶": 1761,
+    "丿": 1409,
+    "艹": 1386,
+    "シ": 1200,
+    "大": 985,
+    "八": 932,
+    "𠆢": 887,
+    "イ": 845,
+    "月": 823,
+    "又": 801,
+    "⺘": 764,
+    "女": 754,
+    "金": 721,
+    "火": 712,
+    "宀": 709,
+    "ク": 657,
+    "亠": 646,
+    "糸": 598,
+    "儿": 596,
+    "虫": 585,
+    "山": 568,
+    "灬": 521,
+    "言": 509,
+    "勹": 505,
+    "厶": 498,
+    "貝": 488,
+    "冖": 487,
+    "心": 475,
+    "⻖": 441,
+    "冂": 432,
+    "⺖": 416,
+    "⺮": 409,
+    "土": 407,
+    "夂": 390,
+    "人": 379,
+    "石": 373,
+    "寸": 361,
+    "隹": 360,
+    "攵": 359,
+    "辶": 350,
+    "鳥": 329,
+    "力": 318,
+    "⺉": 315,
+    "厂": 313,
+    "皿": 291,
+    "艮": 290,
+    "广": 282,
+    "夕": 278,
+    "匕": 265,
+    "尸": 265,
+    "車": 264,
+    "耳": 262,
+    "刀": 261,
+    "几": 255,
+    "子": 255,
+    "巾": 254,
+    "钅": 246,
+    "工": 245,
+    "足": 241,
+    "疒": 234,
+    "止": 234,
+    "丁": 232,
+    "衤": 228,
+    "馬": 224,
+    "方": 221,
+    "⺨": 216,
+    "小": 210,
+    "羽": 208,
+    "立": 206,
+    "雨": 205,
+    "門": 205,
+    "⺫": 200,
+    "彳": 198,
+    "䒑": 197,
+    "廾": 197,
+    "戈": 195,
+    "彡": 192,
+    "酉": 188,
+    "匚": 187,
+    "斤": 186,
+    "欠": 182,
+    "虍": 182,
+    "贝": 169,
+    "弓": 164,
+    "纟": 162,
+    "讠": 160,
+    "幺": 156,
+    "爫": 150,
+    "龷": 140,
+    "示": 138,
+    "⺊": 137,
+    "㐅": 134,
+    "冫": 132,
+    "西": 132,
+    "兀": 129,
+    "豕": 126,
+    "㔾": 125,
+    "⺌": 124,
+    "衣": 122,
+    "ネ": 121,
+    "⺧": 119,
+    "革": 118,
+    "户": 118,
+    "丂": 116,
+    "巳": 114,
+    "臼": 114,
+    "比": 111,
+    "戊": 109,
+    "舟": 107,
+    "巛": 106,
+    "マ": 105,
+    "龰": 104,
+    "辛": 103,
+    "毛": 101,
+    "二": 100
+  };
+
+  totalWeight = 0;
+
+  for (character in availableParts) {
+    weight = availableParts[character];
+    totalWeight += Math.sqrt(weight);
+  }
 
   $.get('data.json', function(data) {
-    var character, tokens;
+    var tokens;
     for (character in data) {
       if (!hasProp.call(data, character)) continue;
       tokens = data[character];
@@ -34,6 +162,18 @@
     return Math.floor(Math.random() * (upper - lower) + lower);
   };
 
+  getRandomParts = function() {
+    var level;
+    level = randint(totalWeight);
+    for (character in availableParts) {
+      weight = availableParts[character];
+      level -= Math.sqrt(weight);
+      if (level < 0) {
+        return character;
+      }
+    }
+  };
+
   resetParts = function(ids) {
     var i, id, len, part, results;
     if (ids == null) {
@@ -42,14 +182,14 @@
     results = [];
     for (i = 0, len = ids.length; i < len; i++) {
       id = ids[i];
-      part = availableParts[randint(availableParts.length)];
+      part = getRandomParts();
       results.push($('.kanji-part').eq(id).text(part).removeClass('active'));
     }
     return results;
   };
 
   submit = function() {
-    var character, hit, parts, partsSize, ref, tokens;
+    var hit, parts, partsSize, ref, tokens;
     parts = [];
     $('.kanji-part').each(function() {
       if ($(this).hasClass('active')) {
